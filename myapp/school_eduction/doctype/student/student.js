4f08ebffc1d9d0df3119197c2dfd7df433473556
fathy <fathy@mail.com>
2025-08-05 16:47:15 +0300

    update1

---
+++ myapp/school_eduction/doctype/student/student.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2025, fathy and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Student", {
+    refresh(frm) {
+
+    },
+    onload: function (frm) {
+        frm.fields_dict['inf_student'].grid.get_field('teacher').get_query = function (doc, cdt, cdn) {
+            return {
+                filters: {
+                    is_active: 1
+                }
+            };
+        };
+    },
+
+});
+
+
+
+frappe.ui.form.on('Information Student', {
+
+    inf_student_add: function (frm) {
+        frappe.show_alert('Hi, you have added a new row!', 5);
+    }
+});
