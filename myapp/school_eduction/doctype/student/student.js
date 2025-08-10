33c76076f672f7d239dc0e87762c0ee684f29a8f
fathy <fathy@mail.com>
2025-08-10 17:10:33 +0300

    update2

--- myapp/school_eduction/doctype/student/student.js
+++ myapp/school_eduction/doctype/student/student.js
@@ -23,5 +23,9 @@ frappe.ui.form.on('Information Student', {
 
     inf_student_add: function (frm) {
         frappe.show_alert('Hi, you have added a new row!', 5);
+    },
+    appreciations: function (frm) {
+
     }
+
 });
--- myapp/school_eduction/doctype/student/student.py
+++ myapp/school_eduction/doctype/student/student.py
@@ -1,9 +1,52 @@
 # Copyright (c) 2025, fathy and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Student(Document):
-	pass
+	
+	def validate (self) :
+		self.validate_id()
+		self.validate_grade()
+
+		frappe.msgprint ("The Full Name is '{0}'".format(
+			(self.first_name or '') + " " + (self.second_name or '') + " " + (self.third_name or '')
+		))
+
+		full_name = (self.first_name or '' ) + " " + (self.second_name or '' ) + " " + (self.third_name or '' ) 
+		self.full_name =full_name
+
+
+
+	def validate_id(self):
+		if self.id:
+			duplicate = frappe.db.exists("Student", {
+				"id": self.id,
+				"name": ["!=", self.name] 
+			})
+
+			if duplicate:
+				frappe.throw(("Student ID '{0}' is already in use by another student.").format(self.id))
+
+    
+
+	def validate_grade(self):
+		if self.inf_student:
+			for row in self.inf_student:
+				if not row.grade: 
+					row.appreciations = ''
+					continue
+
+				grade_value = row.grade
+				if 95 <= grade_value <= 100:
+					row.appreciations = 'Excellent'
+				elif 85 <= grade_value < 95:
+					row.appreciations = 'Very Good'
+				elif 75 <= grade_value < 85:
+					row.appreciations = 'Average'
+				elif 65 <= grade_value < 75:
+					row.appreciations = 'Poor'
+				else:
+					row.appreciations = ''
