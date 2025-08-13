bab4e0a9edac12c547f0d8d9bea155b2b15031a0
fathy <fathy@mail.com>
2025-08-13 15:56:00 +0300

    update3

--- myapp/school_eduction/doctype/student/student.js
+++ myapp/school_eduction/doctype/student/student.js
@@ -2,9 +2,56 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Student", {
-    refresh(frm) {
 
+    refresh: function (frm) {
+        if (!frm.is_new()) {
+            frm.add_custom_button('Add Information', () => {
+                let d = new frappe.ui.Dialog({
+                    title: 'Add Student Information',
+                    fields: [
+                        {
+                            label: 'Teacher',
+                            fieldname: 'teacher',
+                            fieldtype: 'Link',
+                            options: 'Teacher',
+                        },
+                        {
+                            label: 'Subject',
+                            fieldname: 'subject',
+                            fieldtype: 'Data',
+                        },
+                        {
+                            label: 'Appreciations',
+                            fieldname: 'appreciations',
+                            fieldtype: 'Select',
+                            options: ['Excellent', 'Good', 'Average', 'Poor'],
+                        },
+                        {
+                            label: 'Grade',
+                            fieldname: 'grade',
+                            fieldtype: 'Int',
+                        }
+                    ],
+                    primary_action_label: 'Submit',
+                    primary_action(values) {
+                        frm.add_child('inf_student', {
+                            teacher: values.teacher,
+                            subject: values.subject,
+                            appreciations: values.appreciations,
+                            grade: values.grade
+                        });
+
+                        frm.refresh_field('inf_student');
+                        d.hide();
+                    }
+                });
+
+                d.show();
+            });
+        }
     },
+
+
     onload: function (frm) {
         frm.fields_dict['inf_student'].grid.get_field('teacher').get_query = function (doc, cdt, cdn) {
             return {
@@ -14,6 +61,10 @@ frappe.ui.form.on("Student", {
             };
         };
     },
+    after_save: function (frm) {
+
+
+    }
 
 });
 
@@ -24,8 +75,5 @@ frappe.ui.form.on('Information Student', {
     inf_student_add: function (frm) {
         frappe.show_alert('Hi, you have added a new row!', 5);
     },
-    appreciations: function (frm) {
-
-    }
 
 });
